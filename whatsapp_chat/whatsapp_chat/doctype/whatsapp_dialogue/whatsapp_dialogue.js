8528e86592cf086117e4769a9bf4b65b3b16ea29
riekert7 <riekert7@gmail.com>
2025-04-15 22:40:46 +0200

    removed old references for form view modifications

--- whatsapp_chat/whatsapp_chat/doctype/whatsapp_dialogue/whatsapp_dialogue.js
+++ whatsapp_chat/whatsapp_chat/doctype/whatsapp_dialogue/whatsapp_dialogue.js
@@ -4,21 +4,3 @@
 //     },
 // });
 
-// Add button to list view
-frappe.listview_settings['WhatsApp Dialogue'] = {
-    onload: function(listview) {
-        listview.page.add_menu_item(__('Sync Commands to WhatsApp Business API'), function() {
-            frappe.call({
-                method: 'whatsapp_chat.whatsapp_chat.doctype.whatsapp_dialogue.whatsapp_dialogue.sync_commands',
-                callback: function(r) {
-                    if (r.message) {
-                        frappe.show_alert({
-                            message: __('Commands synced successfully'),
-                            indicator: 'green'
-                        });
-                    }
-                }
-            });
-        });
-    }
-}; 
